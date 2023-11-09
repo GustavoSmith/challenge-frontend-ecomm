0a8ca706fdfcc77c99f61cc4afbb70da590c48d4
Gustavo Smith <gustavosmith0010@gmail.com>
2023-11-09 04:59:33 -0300

    Se avanza con el desarrollo del inventario y se realiza la primer petición

--- src/views/inventory/Inventory.tsx
+++ src/views/inventory/Inventory.tsx
@@ -1,4 +1,36 @@
+import { useEffect, useState } from "react";
+import { InventoryItem } from ".";
+
+type InventoryItem = {
+  picture: string;
+  name: string;
+  description: string;
+  stock: number;
+  price: number;
+  id: number;
+};
+
 const Inventory = () => {
+  const [data, setData] = useState<InventoryItem[]>([]);
+
+  useEffect(() => {
+    fetch("https://admin.ecomm-app.com/api/challenge-items", {
+      headers: {
+        Authorization: `Bearer ${
+          import.meta.env.VITE_AUTHORIZATION_TOKEN_BEARER
+        }`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => {
+        console.log(error);
+        return [];
+      });
+  }, []);
+
+  console.log({ data });
+
   return (
     <div className="h-[calc(100vh-3.75rem)] bg-[#ececec] py-3 pl-3 pr-[18px]">
       <div className="flex flex-col gap-3">
@@ -10,19 +42,19 @@ const Inventory = () => {
           las publicaciones de tus canales de venta.
         </p>
       </div>
-      <div className="flex items-center justify-between pb-5 pt-8">
+      <div className="flex w-full justify-between gap-3 pb-5 pt-8 max-[1130px]:flex-col-reverse min-[1130px]:items-center">
         <label
           htmlFor="selectAllItems"
           className="flex items-center gap-1 text-sm/3 text-[#43475F]"
         >
           <input
-            name="selectAllItems"
+            id="selectAllItems"
             type="checkbox"
             className="border-darkBlue-200 text-darkBlue-200 rounded-sm focus:ring-0"
           />
           <span>Seleccionar todos</span>
         </label>
-        <button className="flex items-center rounded-sm bg-[#6097ff] px-2 py-2 text-white hover:bg-[#4871bf]">
+        <button className="ml-auto mr-0 flex w-fit items-center rounded-sm bg-[#6097ff] px-2 py-2 text-white hover:bg-[#4871bf] ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 18 18"
@@ -31,24 +63,57 @@ const Inventory = () => {
             stroke="currentColor"
             className="h-5 w-8"
           >
-            <circle cx="9" cy="9" r="8.57" stroke="white" stroke-width="0.86" />
+            <circle cx="9" cy="9" r="8.57" stroke="white" strokeWidth="0.86" />
             <path
               d="M9.125 4.77881V13.9712"
               stroke="white"
-              stroke-width="0.86"
-              stroke-linecap="round"
+              strokeWidth="0.86"
+              strokeLinecap="round"
             />
             <path
               d="M13.7212 9.375L4.5288 9.375"
               stroke="white"
-              stroke-width="0.86"
-              stroke-linecap="round"
+              strokeWidth="0.86"
+              strokeLinecap="round"
             />
           </svg>
           <span>Nuevo producto</span>
         </button>
       </div>
-      <div></div>
+
+      <div className="flex flex-col gap-3">
+        {data.length === 0 ? (
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              className="m-auto h-12 w-12 animate-spin fill-blue-600 text-gray-200"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        ) : (
+          data.map((item) => (
+            <InventoryItem
+              key={item.id}
+              title={item.name}
+              stock={item.stock}
+              price={item.price}
+              picture={item.picture}
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 };
--- src/views/inventory/InventoryItem.tsx
+++ src/views/inventory/InventoryItem.tsx
@@ -1,7 +1,130 @@
-const InventoryItem = () => {
-  return (
-    <div>InventoryItem</div>
-  )
-}
+type ItemProps = {
+  title: string;
+  stock: number;
+  price: number;
+  picture: string;
+};
 
-export default InventoryItem
+const InventoryItem = ({ title, stock, price, picture }: ItemProps) => {
+  return (
+    <div className="flex min-h-[8.25rem] gap-3 rounded-sm bg-white p-3 shadow-[2px_2px_15px_0_rgba(0,0,0,0.15)]">
+      <input
+        name="selectItem1"
+        type="checkbox"
+        className="border-darkBlue-200 text-darkBlue-200 rounded-sm focus:ring-0"
+      />
+      <button className="text-darkBlue-200 relative h-[3.75rem] w-[3.75rem]">
+        <img
+          src={picture}
+          alt="Headphone image"
+          className="block h-full w-full"
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 26 26"
+          strokeWidth={1}
+          stroke="currentColor"
+          className="absolute bottom-0 right-0 h-5 w-5 rounded-sm bg-[#D7E5FF] p-px hover:bg-[#b0cbff]"
+        >
+          <g id="Group 37541">
+            <path
+              id="Vector 498"
+              d="M3.62356 4.44828H5.87999C6.9787 4.44828 7.96426 3.75492 8.34458 2.72414C8.7249 1.69335 9.71046 1 10.8092 1H15.3648C16.4601 1 17.4362 1.69107 17.8 2.72414C18.1638 3.7572 19.1399 4.44828 20.2352 4.44828H22.3764C23.8254 4.44828 25 5.62289 25 7.07184V18.3764C25 19.8254 23.8254 21 22.3764 21H3.62356C2.17461 21 1 19.8254 1 18.3764V7.07184C1 5.62289 2.17461 4.44828 3.62356 4.44828Z"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <circle
+              id="Ellipse 77"
+              cx="13"
+              cy="12"
+              r="4.5"
+              stroke="currentColor"
+            />
+          </g>
+        </svg>
+      </button>
+      <div className="grid w-full grid-cols-2 gap-3">
+        <label
+          htmlFor="productTitle"
+          className="flex w-full flex-col max-[1130px]:col-span-full"
+        >
+          <span>Título</span>
+          <input
+            type="text"
+            id="productTitle"
+            defaultValue={title}
+            placeholder="Prueba"
+            className="text-darkBlue-200 focus:border-darkBlue-200 rounded-sm border-[#A1A3AF] text-sm/5 focus:shadow-[0_0_4px_0_#6097FF]"
+          />
+        </label>
+        <div className="flex gap-3">
+          <label htmlFor="productStock" className="flex w-[5.6875rem] flex-col">
+            <span>Stock</span>
+            <input
+              type="number"
+              id="productStock"
+              defaultValue={stock}
+              placeholder="Prueba"
+              className="text-darkBlue-200 focus:border-darkBlue-200 rounded-sm border-[#A1A3AF] text-sm/5 focus:shadow-[0_0_4px_0_#6097FF]"
+            />
+          </label>
+          <label htmlFor="productPrice" className="flex flex-col">
+            <span>Precio</span>
+            <div className="relative flex flex-row">
+              <input
+                type="number"
+                min="1"
+                step="any"
+                id="productPrice"
+                defaultValue={price}
+                placeholder="Prueba"
+                className="text-darkBlue-200 focus:border-darkBlue-200 peer w-28 rounded-sm border-[#A1A3AF] pl-10 text-sm/5 focus:shadow-[0_0_4px_0_#6097FF]"
+              />
+              <div className="absolute top-0 m-auto h-[2.4rem] w-8 bg-[#A1A3AF] text-white peer-focus:bg-[#4871bf]">
+                <span className="absolute left-3 top-2">$</span>
+              </div>
+            </div>
+          </label>
+        </div>
+      </div>
+      <div className="ml-auto mr-0">
+        <svg
+          viewBox="0 0 3 16"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          strokeWidth={0.1}
+          stroke="currentColor"
+          className="hover:text-darkBlue-200 h-[1.6875rem] w-[1.6875rem] rounded-sm bg-[#d7e5ff] px-3 py-[0.3rem] text-[#6097FF] hover:bg-[#b0cbff]"
+        >
+          <g id="Group 38244">
+            <circle
+              id="Ellipse 66"
+              cx="1.45455"
+              cy="1.45455"
+              r="1.45455"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse 67"
+              cx="1.45455"
+              cy="7.99996"
+              r="1.45455"
+              fill="currentColor"
+            />
+            <circle
+              id="Ellipse 68"
+              cx="1.45455"
+              cy="14.5455"
+              r="1.45455"
+              fill="currentColor"
+            />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default InventoryItem;
